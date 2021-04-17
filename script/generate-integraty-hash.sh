b32ae74da6debe0c79a8a96a2f8715c6bea464a6
natacha <nat_bartu@hotmail.com>
2021-04-17 20:40:50 +1000

    add subresource integraty to linked files

---
+++ script/generate-integraty-hash.sh
@@ -0,0 +1,14 @@
+echo "index: "
+cat src/css/index.css | openssl dgst -sha384 -binary | openssl base64 -A
+echo ""
+echo "about: "
+cat src/css/about.css | openssl dgst -sha384 -binary | openssl base64 -A
+echo ""
+echo "blog:"
+cat src/css/blog.css | openssl dgst -sha384 -binary | openssl base64 -A
+echo ""
+echo "contact:"
+cat src/css/contact.css | openssl dgst -sha384 -binary | openssl base64 -A
+echo ""
+echo "portfolio:"
+cat src/css/portfolio.css | openssl dgst -sha384 -binary | openssl base64 -A
